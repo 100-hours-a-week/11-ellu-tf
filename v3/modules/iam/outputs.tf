3aa958e8e60484fc27fc60b515465fe271133bc4
Brian Kim <bumsookim333@naver.com>
2025-05-31 01:03:21 +0900

    refactor: 태그 수정

--- v3/modules/iam/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "app_service_account_email" {
-  description = "The email of the application service account"
-  value       = google_service_account.app_service_account.email
-}
-
-output "db_service_account_email" {
-  description = "The email of the database service account"
-  value       = google_service_account.db_service_account.email
-}
